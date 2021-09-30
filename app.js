5ee8e3f0fb3642fdac54a4b854e772074e15f005
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2021-09-30 13:51:24 -0500

    base for HTML site publishing

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+//const printProfileData = profileDataArr => {
+    // this
+//     for (let i = 0; i < profileDataArr.length; i+= 1) {
+//     console.log(profileDataArr[i]);
+//     }
+// console.log('=============');
+
+// //is the same as 
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+//printProfileData(profileDataArgs);
